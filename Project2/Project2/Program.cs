210856aa1a4d4c183a36e3e5976631fe412a9ffb
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-28 11:51:59 -0300

    Creating CalculationService class

---
+++ Project2/Project2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Project2.Services;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                list.Add(number);
+            }
+
+            CalculationService cs = new CalculationService();
+            int max = cs.Max(list);
+            Console.Write("Max: " + max);
+        }
+    }
+}
---
+++ Project2/README.md
@@ -0,0 +1,7 @@
+## C# .NET 6.0
+
+### Restrictions on generics
+
+
+![ex1](https://user-images.githubusercontent.com/88597534/235173369-a24a544e-92ab-404c-b059-e8e6e87fb10d.png)
+
